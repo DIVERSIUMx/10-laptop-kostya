ebd8830cf889d4d2be2e59da825b92b5d3b7851c
DIVERSIUMx <kostya.pankov2010@gmail.com>
2024-12-04 17:05:44 +0300

    Random Circle Qt

--- RandCirclesQt.py
+++ RandCirclesQt.py
@@ -2,24 +2,28 @@ from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QPoint
 from PyQt6 import uic
+
 import sys
 from random import randint
+from forms import Ui_MainWindow
 
 
-class YellowCircles(QMainWindow):
+class YellowCircles(QMainWindow, Ui_MainWindow):
     def __init__(self):
         self.circle_radius = 0
+        self.col = QColor(0, 0, 0, 0)
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.make_circle)
 
     def make_circle(self):
         self.circle_radius = randint(30, 290)
+        self.col = QColor(randint(0, 255), randint(0, 255), randint(0, 255), 255)
         self.update()
 
     def paintEvent(self, a0):
         painter = QPainter(self)
-        painter.setPen(QColor(255, 240, 3, 255))
+        painter.setPen(self.col)
         painter.drawEllipse(QPoint(300, 300), self.circle_radius, self.circle_radius)
 
 
